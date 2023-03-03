363bd4b1a90542655fe66654e0dcbc713f79e48d
Coni Hinojosa <coni@Conis-MBP.broadband>
2023-03-03 12:37:40 +0000

    created an empty navbar

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,62 +1,120 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./NavTabs.css";
 
 function NavTabs() {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <NavLink
-          to="/"
-          end
-          className={({ isActive }) =>
-            isActive ? "nav-link active" : "nav-link"
-          }
-        >
-          Home
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink
-          to="about"
-          className={({ isActive }) =>
-            isActive ? "nav-link active" : "nav-link"
-          }
-        >
-          About
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink
-          to="blog"
-          className={({ isActive }) =>
-            isActive ? "nav-link active" : "nav-link"
-          }
-        >
-          Blog
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink
-          to="contact"
-          end
-          className={({ isActive }) =>
-            isActive ? "nav-link active" : "nav-link"
-          }
-        >
-          Contact
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink
-          to="contact/learn"
-          className={({ isActive }) =>
-            isActive ? "nav-link active" : "nav-link"
-          }
-        >
-          Learn
-        </NavLink>
-      </li>
-    </ul>
+    <header id="topPage">
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <NavLink
+            to="/"
+            end
+            className={({ isActive }) =>
+              `nav-link navbar-brand ${isActive ? "active" : ""}`
+            }
+          >
+            Connie H.
+          </NavLink>
+          <button
+            className="customToggle navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarResponsive"
+            aria-controls="navbarResponsive"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <NavLink
+                  to="about"
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                >
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#skills">
+                  Skills
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#aboutMe">
+                  About me
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#aboutMe">
+                  Contact me
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+
+    // <ul className="nav nav-tabs">
+    //   <li className="nav-item">
+    //     <NavLink
+    //       to="/"
+    //       end
+    //       className={({ isActive }) =>
+    //         isActive ? "nav-link active" : "nav-link"
+    //       }
+    //     >
+    //       Home
+    //     </NavLink>
+    //   </li>
+    //   <li className="nav-item">
+    //     <NavLink
+    //       to="about"
+    //       className={({ isActive }) =>
+    //         isActive ? "nav-link active" : "nav-link"
+    //       }
+    //     >
+    //       About
+    //     </NavLink>
+    //   </li>
+    //   <li className="nav-item">
+    //     <NavLink
+    //       to="blog"
+    //       className={({ isActive }) =>
+    //         isActive ? "nav-link active" : "nav-link"
+    //       }
+    //     >
+    //       Blog
+    //     </NavLink>
+    //   </li>
+    //   <li className="nav-item">
+    //     <NavLink
+    //       to="contact"
+    //       end
+    //       className={({ isActive }) =>
+    //         isActive ? "nav-link active" : "nav-link"
+    //       }
+    //     >
+    //       Contact
+    //     </NavLink>
+    //   </li>
+    //   <li className="nav-item">
+    //     <NavLink
+    //       to="contact/learn"
+    //       className={({ isActive }) =>
+    //         isActive ? "nav-link active" : "nav-link"
+    //       }
+    //     >
+    //       Learn
+    //     </NavLink>
+    //   </li>
+    // </ul>
   );
 }
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home() {
   return (
-    <div>
+    <div className="container">
       <h1>Home Page</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
