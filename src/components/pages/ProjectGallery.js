546983e1ba7fb816fe437dc028505f9cc273c5bb
Coni Hinojosa <coni@Conis-MBP.broadband>
2023-03-06 13:32:27 +0000

    managed to display 6 cards with a JSON file

--- src/components/pages/ProjectGallery.js
+++ src/components/pages/ProjectGallery.js
@@ -4,15 +4,17 @@ import projectData from "../data/project-data.json";
 
 function ProjectGallery() {
   return (
-    <div className="card-group border-dark rounded-0 px-4 py-5 my-5">
-      {projectData.map((project) => (
-        <Project
-          title={project.title}
-          image={project.image}
-          deployedLink={project.deployedLink}
-          githubLink={project.githubLink}
-        ></Project>
-      ))}
+    <div className="container px-4 py-5 my-5">
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        {projectData.map((project) => (
+          <Project
+            title={project.title}
+            image={project.image}
+            deployedLink={project.deployedLink}
+            githubLink={project.githubLink}
+          ></Project>
+        ))}
+      </div>
     </div>
   );
 }
